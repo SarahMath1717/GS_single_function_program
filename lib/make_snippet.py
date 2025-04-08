8c21c7be14e05f1eab68649ed32d853d3f609048
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-04-08 16:47:18 +0100

    make_snippet_complete

--- lib/make_snippet.py
+++ lib/make_snippet.py
@@ -1,8 +1,14 @@
 # A function called make_snippet that takes a string as an argument and returns the first five words and then a '...' if there are more than that.
 
-# Check if string has more than 5 words
+# Count words in string
 # Return entire string if <5
 #   Identify first 5 items of string if >5
 #   Return string + ... for strings >5
 
-def make_snippet():
+def make_snippet(string):
+    words = string.split(", ")
+    if len(words) > 5:
+        snipped_string = ', '.join(words[:5])
+        return (f"{snipped_string}...")
+    else:
+        return string
--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -1,5 +1,9 @@
 from lib.make_snippet import *
 
-def test_make_snipet_length():
+def test_make_snippet_long():
     result = make_snippet("red, orange, yellow, green, blue, indigo, violet")
-    assert result == f"red, orange, yellow, green, blue..."
+    assert result == "red, orange, yellow, green, blue..."
+
+def test_make_snippet_short():
+    result = make_snippet("blue, purple, pink")
+    assert result == "blue, purple, pink"
