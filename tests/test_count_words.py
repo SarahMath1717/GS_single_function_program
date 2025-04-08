a33cd8d616f0e40f45d638ec867cc9ec29fc40a9
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-04-08 16:58:01 +0100

    start_of_count_words

---
+++ tests/test_count_words.py
@@ -0,0 +1,13 @@
+from lib.count_words import *
+
+def test_count_word():
+    result = count_words("Damnit")
+    assert result == 1
+
+# def test_count_words_without_special_characters():
+#     result = count_words("My coffee is gone")
+#     assert result == 4
+
+# def test_count_words_without_special_characters():
+#     result = count_words("where did I leave my cup of coffee, and is it cold?")
+#     assert result == 12
